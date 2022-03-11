89d7b8e1a7183636ef3f61f3bb567443e6f6f11c
Thomas de Lange <thomas.de.lange@mac.com>
2022-03-11 17:08:12 -0500

    initial commit

---
+++ retest/application.py
@@ -0,0 +1,184 @@
+from socket import socket
+from flask import Flask, redirect, request, escape, render_template
+from flask_socketio import SocketIO, emit, join_room, leave_room
+from html_page import entryPage, gamePage
+from game import Game
+import string
+import random
+
+
+roomIds = []
+games = {} #roomId : Game
+
+def generateNewRoomId():
+    chars = string.ascii_uppercase
+    id = ''.join(random.choice(chars) for i in range(10))
+    while id in roomIds:
+        id = ''.join(random.choice(chars) for i in range(10))
+    return id
+
+
+#should check if input is in an acceptable list
+def handleInput(roomId, playerId, keysPressed):
+    player, gameUpdate = games[roomId].handleInput(playerId, keysPressed)
+    if gameUpdate != None:
+        socketio.emit("game update", {'gameState' : gameUpdate}, to=roomId)
+    return player
+
+
+# EB looks for an 'application' callable by default.
+application = app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret808'
+socketio = SocketIO(application)
+
+
+@application.route("/")
+def index():
+    return entryPage
+
+
+@application.route('/create')
+def create_game():
+    if len(games) < 10:
+        if 'name' in request.args.keys():
+            name = escape(request.args['name'])
+            roomId = generateNewRoomId()
+            roomIds.append(roomId)
+            newGame = Game(roomId=roomId)
+            games[roomId] = newGame
+            return redirect("/game?roomId={0}&name={1}".format(roomId, name))
+        else:
+            return '400'
+    else:
+        return '500'
+
+
+@application.route('/join')
+def join_game():
+    roomId = escape(request.args['existingGameId'])
+    if roomId in roomIds and len(games[roomId].players) < 4:
+        name = escape(request.args['name'])
+        return redirect("/game?roomId={0}&name={1}".format(roomId, name))
+    else:
+        return '400'
+
+
+@application.route('/game')
+def test():
+    return gamePage
+
+
+@socketio.on('connect')
+def connect_client():
+    emit('after connect')
+
+
+@socketio.on('join event')
+def client_join(data):
+    #should have try blocks for phony roomIds
+    try:
+        if 'roomId' in data.keys() and 'name' in data.keys():
+            room = escape(data['roomId'])
+            name = escape(data['name'])
+            game = games[room]
+            if len(game.players) < 4:
+                player = game.addPlayer(name)
+                join_room(room)
+                emit('playerId', {'playerId' : player.playerId, 'playerNumber': player.playerNumber, 'players': game.getPlayerDict()})
+                emit('player added', { 'playerNumber':player.playerNumStr, 'name':player.name, 'x':player.x, 'y':player.y }, to=room)
+        else:
+            return '400'
+    except:
+        return '400'
+
+@socketio.on('playerInput')
+def player_input(data):
+    try:
+        roomId = escape(data['roomId'])
+        playerId = escape(data['playerId'])
+        keysPressed = { 'leftPressed' : False, 'rightPressed': False, 'upPressed' : False, 'downPressed' : False }
+        
+        leftPressed = escape(data['leftPressed'])
+        rightPressed = escape(data['rightPressed'])
+        upPressed = escape(data['upPressed'])
+        downPressed = escape(data['downPressed'])
+        keysPressed['leftPressed'] = leftPressed == 'True'
+        keysPressed['rightPressed'] = rightPressed == 'True'# and not (leftPressed == 'True')
+        keysPressed['upPressed'] = upPressed == 'True'
+        keysPressed['downPressed'] = downPressed == 'True'# and not (upPressed == 'True')
+        player = handleInput(roomId, playerId, keysPressed)
+        emit('input response', { 'x' : player.x, 'y' : player.y })
+    except:
+        return '400'
+
+# run the app.
+if __name__ == "__main__":
+    # Setting debug to True enables debug output. This line should be
+    # removed before deploying a production app.
+    application.debug = False
+    # application.run()
+    socketio.run(application)
+
+# @application.route("/app", methods = ['POST', 'GET'])
+# def app_post():
+#     if request.method == 'POST':
+#         return '{ "response" : "asdf" }'
+#     elif request.method == 'GET':
+#         return '{ "response" : "vsdf" }'
+
+# @application.route("/app/join")
+# def app_join():
+#     if request.method == 'GET':
+#         return ''
+
+# def authenticate(args):
+#     return True
+
+# @socketio.on('connect')
+# def test_connect():
+#     if not authenticate(request.args):
+#         raise ConnectionRefusedError
+#     emit('after connect', {'data':'test connect'})
+
+# @socketio.on('message')
+# def handle_json(message):
+#     print('received message: ' + str(message))
+#     emit('update value', message)
+
+# The names message, json, connect and disconnect are reserved and cannot be used for named events.
+
+#rooms-- join_room() and leave_room() -- Example:
+
+        # from flask_socketio import join_room, leave_room
+
+        # @socketio.on('join')
+        # def on_join(data):
+        #     username = data['username']
+        #     room = data['room']
+        #     join_room(room)
+        #     send(username + ' has entered the room.', to=room)
+
+        # @socketio.on('leave')
+        # def on_leave(data):
+        #     username = data['username']
+        #     room = data['room']
+        #     leave_room(room)
+        #     send(username + ' has left the room.', to=room)
+
+#emit and socketio.emit argument to="<room_name>"
+#Since all clients are assigned a personal room, to address a message to a single client,
+#the session ID of the client can be used as the to argument.
+
+#socketio.emit is different from regular emit, needs no context and broadcast is assumed
+#def broadcast_without_context():
+    #socketio.emit('some event', {'data': 42})
+
+# @socketio.on('slider value changed')
+# def value_changed(message):
+#     print(message)
+#     emit('update value', message, broadcast=True)
+
+
+# def start_emitting():
+#     for i in range(15):
+#         emit('skielbijel', broadcast=True)
---
+++ retest/game.py
@@ -0,0 +1,164 @@
+import random
+import string
+from datetime import datetime as dt
+
+
+PLAYER1X0 = 100
+PLAYER1Y0 = 300
+PLAYER2X0 = 400
+PLAYER2Y0 = 100
+PLAYER3X0 = 600
+PLAYER3Y0 = 300
+PLAYER4X0 = 400
+PLAYER4Y0 = 300
+
+MAX_X = 990
+MAX_Y = 690
+MAX_DX = 5
+MAX_DY = 5
+DDX = 0.7
+DDY = 0.7
+
+T_STEP = 50000 #milliseconds
+
+class Game:
+
+    def __init__(self, roomId):
+        self.roomId = roomId
+        self.timeOfLastBroadcast = dt.now()
+        self.players = {} #playerId : Player
+        self.player1 = None
+        self.player2 = None
+        self.player3 = None
+        self.player4 = None
+
+    def addPlayer(self, name):
+        playerId = self.generateNewPlayerId()
+        # n = len(self.players)
+        if self.player1 == None:
+            player = Player(name, playerId, 1, PLAYER1X0, PLAYER1Y0)
+            self.player1 = player
+            self.players[player.playerId] = player
+        elif self.player2 == None:
+            player = Player(name, playerId, 2, PLAYER2X0, PLAYER2Y0)
+            self.player2 = player
+            self.players[player.playerId] = player
+        elif self.player3 == None:
+            player = Player(name, playerId, 3, PLAYER3X0, PLAYER3Y0)
+            self.player3 = player
+            self.players[player.playerId] = player
+        elif self.player4 == None:
+            player = Player(name, playerId, 4, PLAYER4X0, PLAYER4Y0)
+            self.player4 = player
+            self.players[player.playerId] = player
+        else:
+            return None
+        return player
+
+    def removePlayer(self, playerId):
+        if playerId == self.player1.playerId:
+            self.player1 = None
+        if playerId == self.player2.playerId:
+            self.player2 = None
+        if playerId == self.player3.playerId:
+            self.player3 = None
+        if playerId == self.player4.playerId:
+            self.player4 = None
+        del self.players[playerId]
+
+    def generateNewPlayerId(self):
+        digits = string.digits
+        id = ''.join(random.choice(digits) for i in range(10))
+        while id in self.players.keys():
+            id = ''.join(random.choice(digits) for i in range(10))
+        return id
+
+
+    def getPlayerDict(self):
+        playerDict = {}
+        for playerId in self.players.keys():
+            player = self.players[playerId]
+            playerDict[player.playerNumStr] = { 'name' : player.name, 'x' : player.x, 'y' : player.y }
+        return playerDict
+
+
+    def movePlayer(self, playerId, keysPressed):
+        player = self.players[playerId]
+        #slowing down if no keys pressed
+        # maxing out at 0 or max
+        if keysPressed['leftPressed']:
+            if player.dx > MAX_DX * -1:
+                player.dx -= DDX
+                if player.dx < MAX_DX * -1:
+                    player.dx = MAX_DX * -1
+        elif keysPressed['rightPressed']:
+            if player.dx < MAX_DX:
+                player.dx += DDX
+                if player.dx > MAX_DX:
+                    player.dx = MAX_DX
+        else:
+            if player.dx < 0:
+                player.dx += DDX
+                if player.dx > 0:
+                    player.dx = 0
+            elif player.dx > 0:
+                player.dx -= DDX
+                if player.dx < 0:
+                    player.dx = 0
+        if keysPressed['upPressed']:
+            if player.dy > MAX_DY * -1:
+                player.dy -= DDY
+                if player.dy < MAX_DY * -1:
+                    player.dy = MAX_DY * -1
+        elif keysPressed['downPressed']:
+            if player.dy < MAX_DY:
+                player.dy += DDY
+                if player.dy > MAX_DY:
+                    player.dy = MAX_DY
+        else:
+            if player.dy < 0:
+                player.dy += DDY
+                if player.dy > 0:
+                    player.dy = 0
+            elif player.dy > 0:
+                player.dy -= DDY
+                if player.dy < 0:
+                    player.dy = 0
+        
+        player.x += player.dx
+        player.y += player.dy
+        if player.x > MAX_X:
+            player.x = MAX_X
+        elif player.x < 0:
+            player.x = 0
+        if player.y > MAX_Y:
+            player.y = MAX_Y
+        elif player.y < 0:
+            player.y = 0
+
+        return player
+
+    def checkCollisions(self):
+        pass
+
+    #keysPressed - { 'leftPressed' : bool, 'rightPressed' : bool, etc. }
+    def handleInput(self, playerId, keysPressed):
+        player = self.movePlayer(playerId, keysPressed)
+
+        # if end of time step, check for collisions ?
+        if (dt.now() - self.timeOfLastBroadcast).microseconds >= T_STEP:
+            self.checkCollisions()
+            gameUpdate = self.getPlayerDict()
+
+        return player, gameUpdate
+
+class Player:
+    def __init__(self, name, id, playerNumber, x, y):
+        self.name = name
+        self.playerId = id
+        self.playerNumber = playerNumber
+        self.playerNumStr = 'player' + str(playerNumber)
+        self.x = x
+        self.y = y
+        self.dx = 0
+        self.dy = 0
